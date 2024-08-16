51710c346ebe3fb5b91b4b2814aa645c2e9f9bb7
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-08-16 21:59:42 +0800

    0816

--- recyclerk/src/main/java/com/mozhimen/xmlk/recyclerk/decoration/RecyclerKDecorationGapGrid.kt
+++ recyclerk/src/main/java/com/mozhimen/xmlk/recyclerk/decoration/RecyclerKDecorationGapGrid.kt
@@ -14,26 +14,30 @@ import com.mozhimen.xmlk.recyclerk.decoration.bases.BaseRecyclerKDecoration
  */
 class RecyclerKDecorationGapGrid : BaseRecyclerKDecoration {
 
-
     constructor(gapOuter: Int) : this(gapOuter, gapOuter / 2)
 
     constructor(gapOuter: Int, gapInner: Int) : this(gapOuter, gapInner, gapOuter)
 
-    constructor(gapOuter: Int, gapInner: Int, gapOther: Int) {
+    constructor(gapOuter: Int, gapInner: Int, gapOther: Int) : this(gapOuter, gapInner, gapInner, gapOther)
+
+    constructor(gapOuter: Int, gapInnerHorizontal: Int, gapInnerVertical: Int, gapOther: Int) {
         _gapOuter = gapOuter
-        _gapInner = gapInner
+        _gapInnerHorizontal = gapInnerHorizontal
+        _gapInnerVertical = gapInnerVertical
         _gapOther = gapOther
     }
+
     ////////////////////////////////////////////////////////////////
 
     private var _gapOuter: Int = 0
-    private var _gapInner: Int = 0
+    private var _gapInnerHorizontal: Int = 0
+    private var _gapInnerVertical: Int = 0
     private var _gapOther: Int = 0
 
     ////////////////////////////////////////////////////////////////
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
-        UtilKRecyclerViewWrapper.equilibriumAssignment_ofGridLayoutManager(parent, view, outRect, _gapOuter, _gapInner, _gapOther)
+        UtilKRecyclerViewWrapper.equilibriumAssignment_ofGridLayoutManager(parent, view, outRect, _gapOuter, _gapInnerHorizontal, _gapInnerVertical, _gapOther)
     }
 }
