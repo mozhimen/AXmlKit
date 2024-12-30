0e4e5bc68944743222e6635ae351b77f5d29ce6d
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-12-31 00:54:30 +0800

    1231

--- textk/src/main/java/com/mozhimen/xmlk/textk/TextKStrikethrough.kt
+++ textk/src/main/java/com/mozhimen/xmlk/textk/TextKStrikethrough.kt
@@ -1,10 +1,12 @@
 package com.mozhimen.xmlk.textk
 
 import android.content.Context
+import android.graphics.Paint
 import android.text.TextPaint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 import com.mozhimen.xmlk.commons.IViewK
+
 
 /**
  * @ClassName TextKUnderLine
@@ -13,7 +15,7 @@ import com.mozhimen.xmlk.commons.IViewK
  * @Date 2021/6/10 20:46
  * @Version 1.0
  */
-class TextKUnderLine @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatTextView(context, attrs, defStyleAttr), IViewK {
+class TextKStrikethrough @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatTextView(context, attrs, defStyleAttr), IViewK {
 
     private lateinit var _textPaint: TextPaint//TextPaint对象，继承自Paint
     private var _enabled = false//是否加下划线
@@ -25,9 +27,9 @@ class TextKUnderLine @JvmOverloads constructor(context: Context, attrs: Attribut
 
     override fun initAttrs(attrs: AttributeSet?, defStyleAttr: Int) {
         attrs ?: return
-        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.TextKUnderLine, defStyleAttr, 0)//Load attributes 加载属性列表R.styleable.UnderLineTextView
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.TextKStrikethrough, defStyleAttr, 0)//Load attributes 加载属性列表R.styleable.UnderLineTextView
         _enabled =
-            typedArray.getBoolean(R.styleable.TextKUnderLine_textKUnderLine_enabled, false)        //获取自定义属性,默认是false
+            typedArray.getBoolean(R.styleable.TextKStrikethrough_textKStrikethrough_enabled, false)        //获取自定义属性,默认是false
         typedArray.recycle()
     }
 
@@ -35,7 +37,7 @@ class TextKUnderLine @JvmOverloads constructor(context: Context, attrs: Attribut
         _textPaint = paint
         if (_enabled) {
             //设置下划线
-            _textPaint.isUnderlineText = true
+            _textPaint.flags = Paint.STRIKE_THRU_TEXT_FLAG or Paint.ANTI_ALIAS_FLAG
         }
     }
 }
